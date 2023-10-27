437e6dd91f4484f7efc5be233c20e41aff88540b
Israel Calderon de la Cruz <icalderond@outlook.com>
2023-10-27 06:52:50 -0600

    + rss service

---
+++ FeedBag/FeedBag/RSSService.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using FeedBag.Models;
+
+namespace FeedBag
+{
+    public class RSSService
+    {
+        private readonly WebClient _webClient;
+        private readonly string _rssWorldUrlString;
+
+        public RSSService()
+        {
+            _webClient = new WebClient();
+            _rssWorldUrlString = @"https://www.theguardian.com/world/rss";
+        }
+
+        public async Task<List<FeedNew>> GetNews()
+        {
+            var xmlString = await _webClient.DownloadStringTaskAsync(_rssWorldUrlString);
+
+            XDocument xDocument = XDocument.Parse(xmlString);
+            XNamespace media = xDocument.Root.GetNamespaceOfPrefix("media");
+            XNamespace dc = xDocument.Root.GetNamespaceOfPrefix("dc");
+
+            var elements = (from news in xDocument.Descendants("channel").Elements("item")
+                            select new FeedNew
+                            {
+                                Title = news.Element("title").Value,
+                                Description = news.Element("description").Value,
+                                Link = news.Element("link").Value,
+                                UrlImage = news.Elements(media + "content").Last().Attribute("url").Value,
+                                PubDate = news.Element("pubDate").Value,
+                                Creator = news.Element(dc + "creator").Value,
+                                Category = news.Elements("category").Last().Value
+                            }).ToList();
+
+            return elements;
+        }
+    }
+}
+
--- FeedBag/FeedBag/ViewModels/NewsViewModel.cs
+++ FeedBag/FeedBag/ViewModels/NewsViewModel.cs
@@ -27,20 +27,16 @@ namespace FeedBag.ViewModels
             set => Set(ref _FeedNewItem, value);
         }
 
-        public void Initialize()
+        private RSSService _RSSService;
+        public RSSService RSSService
         {
-            var _feedNewList = new List<FeedNew>();
-            for (int i = 0; i < 10; i++)
-            {
-                _feedNewList.Add(new FeedNew
-                {
-                    Title = "Girls in Africa quitting school over cost of living crisis, says charity",
-                    Description = $"<p>Camfed calls for six-year plan to get 6 million girls into school, warning that drop-out rate is limiting children’s chances</p><p>Governments and donors need to redouble efforts to encourage girls back to school across Africa after the cost of living crisis pushed many to spurn education for low-paid work or early marriage, a charity has warned.</p><p>Camfed, which operates in five African countries, said its partnership model proved this could be achieved and <a href=\"https://camfed.org/camfed-launches-vision-for-2030/\">called for a six-year plan</a> to get 6 million girls into school.</p> <a href=\"https://www.theguardian.com/global-development/2023/oct/25/girls-in-africa-quitting-school-over-cost-of-living-crisis-says-charity\">Continue reading...</a>",
-                    UrlImage = "https://i.guim.co.uk/img/media/49a7f25a3ef7ecf19b33243a002a34b9cb80a830/0_26_4823_2894/master/4823.jpg?width=460&quality=85&auto=format&fit=max&s=77d735d9b4ab6af6708c85547314746b",
-                    Creator = "Phillip Inman",
-                    PubDate = DateTime.Now.ToShortDateString()
-                });
-            }
+            get => _RSSService = _RSSService ?? new RSSService();
+            set => _RSSService = value;
+        }
+
+        public async void Initialize()
+        {
+            var _feedNewList = await RSSService.GetNews();
 
             FeedNewItem = _feedNewList.FirstOrDefault();
             FeedNewList = new ObservableCollection<FeedNew>(_feedNewList.GetRange(1, _feedNewList.Count - 1));
